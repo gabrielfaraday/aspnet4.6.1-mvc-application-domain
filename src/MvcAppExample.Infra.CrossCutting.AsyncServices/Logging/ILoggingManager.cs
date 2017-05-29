5a6869537565f76cccfa13f1110d25f04ab79711
Gabriel Faraday de Barros | S2IT <gabriel.barros@s2it.com.br>
2017-05-29 17:47:29 -0300

    Inicia configuracao para logs.

---
+++ src/MvcAppExample.Infra.CrossCutting.AsyncServices/Logging/ILoggingManager.cs
@@ -0,0 +1,28 @@
+﻿namespace MvcAppExample.Infra.CrossCutting.AsyncServices.Logging
+{
+    public interface ILoggingManager
+    {
+        void DebugLog(string message);
+        void InfoLog(string message);
+        void WarnLog(string message);
+        void ErrorLog(string message);
+        void FatalLog(string message);
+
+
+        //public class ManagerLog : IManagerLog
+        //{
+        //    private ILog _logger;
+        //    public ManagerLog()
+        //    {
+        //        _logger = LogManager.GetLogger("ExampleLog");
+        //        log4net.Config.XmlConfigurator.Configure();
+        //    }
+
+        //    public void DebugLog(string message) => Task.Run(() => _logger.Debug(message));
+        //    public void InfoLog(string message) => Task.Run(() => _logger.Info(message));
+        //    public void WarnLog(string message) => Task.Run(() => _logger.Warn(message));
+        //    public void ErrorLog(string message) => Task.Run(() => _logger.Error(message));
+        //    public void FatalLog(string message) => Task.Run(() => _logger.Fatal(message));
+        //}
+    }
+}
--- src/MvcAppExample.Web/Startup.cs
+++ src/MvcAppExample.Web/Startup.cs
@@ -2,6 +2,7 @@
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(MvcAppExample.Web.Startup))]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
 namespace MvcAppExample.Web
 {
     public partial class Startup
